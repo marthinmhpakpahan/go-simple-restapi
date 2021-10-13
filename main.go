8d4d4487e8859e704ac811b8f458515c0ae72290
Marthin <martin.pakpahan@edgeprop.sg>
2021-10-13 22:53:03 +0700

    handle param for detail api

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 )
 
 type Article struct {
+	Id string `json:"Id"`
 	Title string `json:"Title"`
 	Desc string `json:"Desc"`
 	Content string `json:"Content"`
@@ -21,6 +22,22 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: homePage")
 }
 
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
 func handleRequest() {
 	// creates a new instance of a mux router
 	myRouter := mux.NewRouter().StrictSlash(true)
@@ -28,22 +45,18 @@ func handleRequest() {
 	// replace http.HandleFunc with myRouter.HandleFunc
     myRouter.HandleFunc("/", homePage)
     myRouter.HandleFunc("/all", returnAllArticles)
+    myRouter.HandleFunc("/article/{id}", returnSingleArticle)
 
     // finally, instead of passing in nil, we want
     // to pass in our newly created router as the second argument
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnAllArticles")
-	json.NewEncoder(w).Encode(Articles)
-}
-
 func main() {
 	fmt.Println("Rest API v2.0 - Mux Routers")
 	Articles = []Article {
-		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-        Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+		Article{Id:"1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+        Article{Id:"2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
 	handleRequest()
 }
