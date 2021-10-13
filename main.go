430f954262f7350c26c9255dd87a16abe642203b
Marthin <martin.pakpahan@edgeprop.sg>
2021-10-13 18:23:31 +0700

    add new endpoint to return all articles

--- main.go
+++ main.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 )
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"Desc"`
+	Content string `json:"Content"`
+}
+
+var Articles []Article
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage")
@@ -13,9 +22,19 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
 func main() {
+	Articles = []Article {
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+        Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
 	handleRequest()
 }
